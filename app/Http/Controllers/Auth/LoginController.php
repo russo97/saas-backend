b105f34c97118364b340b5d40b27850386885624
Wanderson Silva <wandersonsilva768@gmail.com>
2023-05-16 00:50:31 -0300

    feat: realizando autenticação do usuário

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
-class LoginController extends Controller
-{
-    //
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller {
+  public function __invoke (LoginRequest $request) {
+    $input = $request -> validated();
+
+    if (!Auth::attempt($input)) {}
+
+    return auth() -> user();
+  }
 }
